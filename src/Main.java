d591751d626d95b30f27a3bcbf331e5dd6434f2c
Conor-Developer <c_hope@hotmail.co.uk>
2022-08-08 15:56:48 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        // Calculates diameter and circumference
+        double circleRadius = 12.0;
+        double circleDiameter = circleRadius * 2;
+        double circleCircumference = circleDiameter * 3.14;
+
+        System.out.println(circleRadius);
+        System.out.println(circleDiameter);
+        System.out.println(circleCircumference);
+    }
+}
